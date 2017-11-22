141b224cd62136e8901ee7ccf6997f72cdc800e8
y.kotseba <ykotseba@cybervisontech.com>
2017-11-22 15:58:20 +0200

    Added not commited test files

--- app/src/androidTest/java/com/example/user013050/justjava/FirstTest.java
+++ app/src/androidTest/java/com/example/user013050/justjava/FirstTest.java
@@ -1,8 +1,36 @@
 package com.example.user013050.justjava;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * Created by user013050 on 22.11.17.
  */
+@android.test.suitebuilder.annotation.LargeTest
+@RunWith(AndroidJUnit4.class)
 
 public class FirstTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+    @Test
+    public void test1ChatId(){
+        onView(withText("TOPPINGS")).check(matches(isDisplayed()));
+    }
 }
--- app/src/main/java/com/example/user013050/justjava/MainActivity.java
+++ app/src/main/java/com/example/user013050/justjava/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.user013050.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
+
+import static com.example.user013050.justjava.R.id.decrease_quantity_button;
 
 
 /**
@@ -19,32 +27,78 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public static int quantity = 1;
+    public static int whippedCreamPrice = 1;
+    public static int chocolateToppingPrice = 2;
+    public static int coffeeCupPrice = 5;
+    public static boolean checked;
+    public static boolean checked1;
+    public static String name;
+
     /**
      * This method is called when quantity increment button is clicked.
      */
-    public static int quantity = 0;
-
     public void increment(View view){
+        Button decreaseButton = (Button)findViewById(R.id.decrease_quantity_button);
+        Button increaseButton = (Button)findViewById(R.id.increase_quantity_button);
+        if (quantity >= 1){
+            decreaseButton.setEnabled(true);
+        }
+        if (quantity >=99){
+            increaseButton.setEnabled(false);
+        }
         display(quantity=quantity+1);
     }
 
     /**
      * This method is called when quantity decrement button is clicked
      */
-    public void decrement(View view){
+    public void decrement(View view) {
+        Button decreaseButton = (Button)findViewById(R.id.decrease_quantity_button);
+        if (quantity <=2){
+            decreaseButton.setEnabled(false);
+        }
         display(quantity=quantity-1);
     }
 
     /**
-     * This method is called when the order button is clicked.
+     * This method is called when the order button is clicked, displays total order info
      */
-
     public void submitOrder(View view) {
-
-
-        displayPrice(quantity*5);
+            EditText sometext = (EditText) findViewById(R.id.add_name_text_input);
+            name = sometext.getText().toString();
+//        String priceMessage = "Name: " + name + "\nAdd whipped cream? " + checked + "\nAdd chocolate? " + checked1 + "\nQuantity: " + quantity + "\nTotal: " + "$" + quantity * coffeeCupPrice + "\nThank you!";
+//        displayMessage(priceMessage);
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("*/*");
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order to " + name);
+            intent.putExtra(Intent.EXTRA_TEXT, "Name: " + name + "\nAdd whipped cream? " + checked + "\nAdd chocolate? " + checked1 + "\nQuantity: " + quantity + "\nTotal: " + "$" + quantity * coffeeCupPrice + "\nThank you!");
+            if (intent.resolveActivity(getPackageManager()) != null){
+                startActivity(intent);
+            }
     }
 
+    /**
+     * This method saves state of whipped cream checkbox
+     */
+    public void onCheckboxClicked (View view){
+            checked = ((CheckBox) view).isChecked();
+
+            if (checked == true){
+                coffeeCupPrice = coffeeCupPrice +whippedCreamPrice;
+            }
+
+    }
+
+    /**
+     * This method saves state of chocolate checkbox
+     */
+    public void hasChocolate (View view){
+        checked1 = ((CheckBox) view).isChecked();
+            if (checked1 == true){
+                coffeeCupPrice = coffeeCupPrice +chocolateToppingPrice;
+            }
+    }
     /**
      * This method displays the given quantity value on the screen.
      */
@@ -60,4 +114,13 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_count_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_count_text_view);
+        priceTextView.setText(message);
+    }
+
 }
